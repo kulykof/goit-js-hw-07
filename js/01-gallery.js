b0731d25eca435a787bb446dea545c34d4d058cb
Oleksandr <kulykof@gmail.com>
2022-11-04 01:08:33 +0200

    add task 1 and task 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const refs = {
+    galleryParent: document.querySelector(".gallery"),
+};
+
+refs.galleryParent.addEventListener("click", onModalOpen);
+
+makeGalleryMarkup(galleryItems, refs.galleryParent);
+
+function makeGalleryMarkup(gallery, parent) {
+    const galleryMarkup = gallery
+        .map((img) => {
+            return `<div class="gallery__item">
+        <a class="gallery__link" href='${img.original}'>
+            <img
+                class="gallery__image"
+                src='${img.preview}'
+                data-source='${img.original}'
+                alt='${img.description}'
+            />
+        </a></div>`;
+        })
+    .join("");
+
+    parent.innerHTML = galleryMarkup;
+}
+
+function onModalOpen(e) {
+    e.preventDefault();
+
+    let libraryImgMarkup = null;
+
+    if (e.target.classList.contains("gallery__image")) {
+        e.target.src = e.target.dataset.source;
+        libraryImgMarkup = e.target.parentNode.innerHTML;
+    } else if (e.target.classList.contains("gallery__link")) {
+        e.target.src = e.target.dataset.source;
+        libraryImgMarkup = e.target.innerHTML;
+    }
+
+    if (libraryImgMarkup) {
+        const instance = basicLightbox.create(libraryImgMarkup);
+
+        instance.show();
+
+    const onEscBtnModalClose = function (e) {
+        if (e.code === "Escape") {
+            document.removeEventListener("keydown", onEscBtnModalClose);
+            instance.close();
+        }
+    };
+
+        document.addEventListener("keydown", onEscBtnModalClose);
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const refs = {
+    galleryParent: document.querySelector(".gallery"),
+};
+
+makeGalleryMarkup(galleryItems, refs.galleryParent);
+
+function makeGalleryMarkup(gallery, parent) {
+    parent.innerHTML = gallery
+        .map((img) => {
+            return `<a class="gallery__item" href='${img.original}'>
+                    <img class="gallery__image" src='${img.preview}' alt='${img.description}' />
+                    </a>`;
+        })
+        .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionType: "alt",
+    captionsData: "alt",
+    captionDelay: 250,
+});
